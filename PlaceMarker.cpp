316cd81eb889b8b3a39679b39e3225db53827954
jrubtsov <46731582+jrubtsov@users.noreply.github.com>
2021-02-16 17:36:25 -0700

    Adding PlaceMarker.cpp

---
+++ PlaceMarker.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+char curr_mark;
+int curr_player;
+
+//gets row and column based on input square
+void PlaceMarker(int square){
+	int row = square/3;
+	int col;
+	
+	//if square is multiple of 3
+	if (square%3 == 0){
+		row = row - 1;
+		col = 2;
+	}
+	else{
+		col = (square%3) - 1;
+	}
+	
+	board[row][col] = curr_mark;
+}
+
+int main(){
+	
+}
