ad7a8a0daf504d774d891cb675c3787f30b973b7
jayu2394 <jayu2394@colorado.edu>
2021-02-16 18:27:42 -0700

    changes to tictactoe file

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,17 +1,49 @@
 #include <iostream> 
 
-int CreateBoard(){
-    int board [3][3] = {(0,0),(0,0),(0,0)};
-    return board;
+std::string board[3][3] = {(" ", " ", " "),(" "," "," "),(" "," "," ")};
+
+// std::string CreateBoard(){
+//     std::string board[3][3] = {(" ", " ", " "),(" "," "," "),(" "," "," ")};
+//     return board[3][3];
+// }
+
+
+void DisplayBoard(std::string board[3][3]){
+	std::cout << std::endl;
+    std::cout << " " << board[0][0] << "|" << board[0][1] << "|" << board[0][2] << std::endl;
+    std::cout << "--|-|--" << std::endl;
+    std::cout << " " << board[1][0] << "|" << board[1][1] << "|" << board[1][2] << std::endl;
+    std::cout << "--|-|--" << std::endl;
+    std::cout << " " << board[2][0] << "|" << board[2][1] << "|" << board[2][2] << std::endl;
+	std::cout << std::endl;
 }
 
 
-void DisplayBoard(){
-    std::cout<<"   |   |   "<<std::endl;
-    std::cout<<"-----------"<<std::endl;
-    std::cout<<"   |   |   "<<std::endl;
-    std::cout<<"-----------"<<std::endl;
-    std::cout<<"   |   |   "<<std::endl;
+
+// void DisplayBoard(){
+//     std::cout<<"   |   |   "<<std::endl;
+//     std::cout<<"-----------"<<std::endl;
+//     std::cout<<"   |   |   "<<std::endl;
+//     std::cout<<"-----------"<<std::endl;
+//     std::cout<<"   |   |   "<<std::endl;
+// }
+
+void PlaceMarker(int square, char curr_mark){
+    
+	int row = square/3;
+	int col;
+	
+	//if square is multiple of 3
+	if (square%3 == 0){
+		row = row - 1;
+		col = 2;
+	}
+	else{
+		col = (square%3) - 1;
+	}
+    
+	board[row][col] = curr_mark;
+
 }
 
 int GetPlayerChoice(int turn){
@@ -37,7 +69,29 @@ int GetPlayerChoice(int turn){
 }
 
 int main(){
-    DisplayBoard();
-    int choice = GetPlayerChoice(0); 
-    std::cout<<choice;
+    int check = 0; 
+    int turn = 0;
+    int counter = 0;
+//     std::string b = CreateBoard();
+    while(check == 0){
+        int choice;
+        choice = GetPlayerChoice(turn);
+        
+        if(turn == 0){
+            PlaceMarker(choice,'X');
+            turn = 1;
+        }
+        else{
+            PlaceMarker(choice,'O');
+            turn = 0;
+        }
+        
+        DisplayBoard(board);
+        counter++;
+        
+        if(counter == 9){
+            check = 1; 
+        }
+        
+    }
 }
